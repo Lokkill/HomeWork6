5e4ca75a2b6f6db1d993a6d79b3fb82581143e3c
Андрей Катаев <crovolk@yandex.ru>
2020-10-15 10:39:59 +0300

    Home work is done.

--- src/ru/ourcompany/Animal.java
+++ src/ru/ourcompany/Animal.java
@@ -1,7 +1,59 @@
 package ru.ourcompany;
 
-import java.util.Random;
-
 public abstract class Animal {
+    private final String name;
+    private final String color;
+    private final int age;
+    private double heightJump;
+    private int lengthRun;
+    private int lengthSwim;
 
+    Animal(String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.printf("Имя: %s\n"
+        + "Цвет: %s\n"
+        + "Возраст: %d\n"
+        + "Максимальаня высота прыжка: %.1f\n"
+        + "Максимальная длина бега: %d\n"
+        + "Максимальная длина плавания: %d\n",
+                name, color, age, heightJump, lengthRun, lengthSwim);
+    }
+
+    public void run(int value){
+        System.out.println(name + " run: " + (value <= lengthRun));
+    }
+
+    public void swim(int value){
+        if (this instanceof Cat){
+            System.out.println(name + " swim: " + false);
+        } else {
+            System.out.println(name + " swim: " + (value <= lengthSwim));
+        }
+    }
+
+    public void jump(int value){
+        System.out.println(name + " jump: " + (value <= heightJump));
+    }
+
+    public double rangeRandom(double min, double max, int multiply){
+
+        return ((Math.random() * ((max - min) + 1)) + min) * (double)multiply;
+    }
+
+    public void setHeightJump(double heightJump) {
+        this.heightJump = heightJump;
+    }
+
+    public void setLengthRun(int lengthRun) {
+        this.lengthRun = lengthRun;
+    }
+
+    public void setLengthSwim(int lengthSwim) {
+        this.lengthSwim = lengthSwim;
+    }
 }
